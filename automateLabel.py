96f52036cf4d158dba814019b46622c2ddacc331
Prathamesh Dhande <prathameshdhande534@gmail.com>
2023-01-18 19:41:40 +0530

    automate file names

---
+++ automateLabel.py
@@ -0,0 +1,8 @@
+import clipboard
+
+filename=input("Enter the File Name : ").replace(".","").split(" ")
+fileno=filename[0]
+filename.remove(fileno)
+filename.append(fileno)
+
+clipboard.copy('_'.join(filename))
