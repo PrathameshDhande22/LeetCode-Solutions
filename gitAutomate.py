31374161e39f44977895d6ffd9caacfc460009cb
Prathamesh Dhande <prathameshdhande534@gmail.com>
2023-03-07 20:13:56 +0530

    gitautomation

--- gitAutomate.py
+++ gitAutomate.py
@@ -1,6 +1,7 @@
 from git import repo
 import os
 from git import repo
+from git import remote
 Current_Dir=os.getcwd()
 reps=repo.Repo(path=Current_Dir)
 untrack=reps.untracked_files
@@ -8,7 +9,7 @@ for i in untrack:
     reps.index.add(i)
     commit_msg="Leetcode "+i.split(".")[0].split("_").pop()
     reps.index.commit(message=commit_msg)
-    
+reps.remote().push()
 # rep=cmd.Git(Current_Dir)
 # print(rep.execute(command=["git","status"],stdout_as_string=True).split(""))
 
