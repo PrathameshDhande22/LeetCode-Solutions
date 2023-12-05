04f20ec2a3c77a6cdb1d913028245609568f3dad
Prathameshdhande22 <prathameshdhande534@gmail.com>
2023-12-05 10:21:06 +0530

    Leetcode 263

---
+++ Ugly_Number_263.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+class Solution263 {
+    public boolean isUgly(int n) {
+        if (n == 1) {
+            return true;
+        }
+        if (n <= 0) {
+            return false;
+        }
+
+        if (n % 2 == 0) {
+            return isUgly(n / 2);
+        } else if (n % 3 == 0) {
+            return isUgly(n / 3);
+        } else if (n % 5 == 0) {
+            return isUgly(n / 5);
+        }
+        return false;
+    }
+}
+
+public class Ugly_Number_263 {
+    public static void main(String[] args) {
+        int n = 701;
+        Solution263 s = new Solution263();
+        System.out.println(s.isUgly(n));
+    }
+}
