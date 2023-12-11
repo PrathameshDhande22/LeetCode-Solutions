475fa7dd8b68ebbc37322fc1d449faf1a6e02eb0
Prathameshdhande22 <prathameshdhande534@gmail.com>
2023-12-11 16:18:51 +0530

    Leetcode 141

---
+++ Linked_List_Cycle_141.java
@@ -0,0 +1,41 @@
+package leetcode;
+
+class ListNode {
+	int val;
+	ListNode next;
+
+	ListNode(int x) {
+		val = x;
+		next = null;
+	}
+}
+
+class Solution141 {
+	public boolean hasCycle(ListNode head) {
+		ListNode slow = head;
+		ListNode fast = head;
+		while (fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+			if (slow == fast) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
+
+public class Linked_List_Cycle_141 {
+
+	public static void main(String[] args) {
+		ListNode head = new ListNode(3);
+		head.next = new ListNode(2);
+		head.next.next = new ListNode(0);
+		head.next.next.next = new ListNode(-4);
+		head.next.next.next.next = head.next;
+		Solution141 ns = new Solution141();
+		System.out.println(ns.hasCycle(head));
+
+	}
+
+}
