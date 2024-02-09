95600a0b697f8fc064e2e346f771e28026242951
Prathameshdhande22 <prathameshdhande534@gmail.com>
2024-02-09 20:08:56 +0530

    Removed the ListNode class to its own file

--- Linked_List_Cycle_141.java
+++ Linked_List_Cycle_141.java
@@ -1,15 +1,5 @@
 package leetcode;
 
-class ListNode {
-	int val;
-	ListNode next;
-
-	ListNode(int x) {
-		val = x;
-		next = null;
-	}
-}
-
 class Solution141 {
 	public boolean hasCycle(ListNode head) {
 		ListNode slow = head;
